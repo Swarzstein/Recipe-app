b3dac3dc475e0cf6c6d36bcb912df2cba8daa0fd
Swarzstein <zorrillagja@gmail.com>
2023-07-11 14:44:19 -0400

    schema created and 1st modifications of routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :inventories
   resources :recipes
   resources :users
+
+  root to: "recipes#index", as: "public_recipes"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
