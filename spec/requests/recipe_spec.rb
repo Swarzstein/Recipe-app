2d37783718702e179d950533af304af47c7c8f4a
everinurmind <nurbol.sultanov@gmail.com>
2023-07-12 18:28:06 -0600

    Added user model test units

--- spec/requests/recipe_spec.rb
+++ spec/requests/recipe_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Recipes", type: :request do
-  describe "GET /index" do
-    let(:current_user) { User.create(name: "Akai", email: "akai123@gmail.com", password: "123456") }
-    
+RSpec.describe 'Recipes', type: :request do
+  describe 'GET /index' do
+    let(:current_user) { User.create(name: 'Akai', email: 'akai123@gmail.com', password: '123456') }
+
     before do
       current_user.confirm
       sign_in current_user
     end
 
-    it "returns http success" do
+    it 'returns http success' do
       get recipes_path
       expect(response).to have_http_status(:success)
     end
