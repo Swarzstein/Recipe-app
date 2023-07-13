07daf6ef050fc751d6a4b8b80672310292d0bd21
Swarzstein <zorrillagja@gmail.com>
2023-07-13 16:29:47 -0400

    tests for shopping list and recipe show created

--- spec/requests/recipe_spec.rb
+++ spec/requests/recipe_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe 'Recipes', type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'GET /show' do
+    let(:current_user) { User.create(name: 'Akai', email: 'akai123@gmail.com', password: '123456') }
+    let(:recipe) { Recipe.create(name: 'Recipe 2', description: 'This is a test and does nothing else', cooking_time: '20', preparation_time: '10', public: false, user_id: current_user.id) }
+
+    before do
+      current_user.confirm
+      sign_in current_user
+    end
+
+    it 'returns http success' do
+      get recipe_path(recipe)
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
